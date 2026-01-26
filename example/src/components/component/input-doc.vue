20a6ef4f9e5c0693e1b6e48cc7862a33f4dcb1f2
cqcpcqp <qi.chen@transwarp.io>
2026-01-26 14:54:13 +0800

    feat(ui): input 密码框

--- example/src/components/component/input-doc.vue
+++ example/src/components/component/input-doc.vue
@@ -2,16 +2,14 @@
 export default {
   data() {
     return {
-      withInitialValue: 'Initial Value',
+      withInitialValue: '',
       placeholderValue: '',
       nullValue: null,
       undefinedValue: undefined,
       placeholder: 'placeholder',
-      size: 'md',
-      sizeList: ['sm', 'md', 'lg'],
     };
   },
-  mounted() {},
+  mounted() { },
 };
 </script>
 
@@ -19,39 +17,24 @@ export default {
   <div class="input-doc">
     <h1>Input</h1>
 
-    <p>输入文本内容</p>
-
-    <ds-flex gap="20px">
-      <ds-button v-for="s in sizeList" category="primary" @click="size = s">
-        {{ s }}
-      </ds-button>
-    </ds-flex>
-
     <ds-flex :direction="'column'" gap="20px">
-      <p>具有初始值的ds-input / {{ withInitialValue }}</p>
-      <ds-input :placeholder="placeholder" :size="size" v-model="withInitialValue"></ds-input>
+      <h2>尺寸</h2>
+      <ds-input placeholder="small" size="sm" v-model="withInitialValue"></ds-input>
+      <ds-input placeholder="medium" size="md" v-model="withInitialValue"></ds-input>
+      <ds-input placeholder="large" size="lg" v-model="withInitialValue"></ds-input>
 
-      <!-- <p>没有有初始值展示placeholder的ds-input/ {{ placeholderValue }}</p>
-      <ds-input placeholder="placeholder" v-model="placeholderValue" size="sm"></ds-input>
+      <h2>密码框</h2>
+      <ds-input :type="'password'" placeholder="password" size="md" v-model="withInitialValue"></ds-input>
 
-      <p>nullValue / {{ nullValue }}</p>
-      <ds-input placeholder="placeholder" v-model="nullValue" size="sm"></ds-input>
-
-      <p>undefinedValue / {{ undefinedValue }}</p>
-      <ds-input placeholder="placeholder" v-model="undefinedValue" size="sm"></ds-input> -->
-
-      <p>以下为原生 文本输入功能</p>
+      <h2>原生</h2>
       <input type="text" style="height: 32px; line-height: 22px" v-model="withInitialValue" />
 
       <input type="text" style="height: 32px; line-height: 22px" v-model="placeholder" />
 
       <fieldset>
         <legend>contenteditable div</legend>
-        <div
-          contenteditable
-          placeholder="Basic Usage"
-          style="outline: 1px solid grey; width: 300px; padding: 4px 8px"
-        ></div>
+        <div contenteditable placeholder="Basic Usage" style="outline: 1px solid grey; width: 300px; padding: 4px 8px">
+        </div>
       </fieldset>
     </ds-flex>
   </div>
